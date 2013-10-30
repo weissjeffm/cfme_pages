76bec8cec0fafc3e5e7917a3a57b48699777f266
Jeff Weiss <jweiss@redhat.com>
2013-10-30 14:13:00 -0400

    Use context manager to start/stop sel

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,21 @@
 import pytest
 import threading
 from selenium import webdriver
+from contextlib import contextmanager
 
 thread_locals = threading.local()
 
+@contextmanager
+def selenium_session(cls):
+    thread_locals.selenium = object.__new__(cls)
+    thread_locals.selenium.__init__()
+    yield thread_locals.selenium
+    thread_locals.selenium.quit()
+
 @pytest.yield_fixture
-def selenium(request):
-    thread_locals.selenium = webdriver.Firefox()
-    try:
-        yield thread_locals.selenium
-    finally:
-        thread_locals.selenium.quit()
+def selenium():
+    with selenium_session(webdriver.Firefox) as sel:
+        yield sel
 
 
 
